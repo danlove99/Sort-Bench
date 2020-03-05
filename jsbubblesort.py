107b3c20706cae871acbf81a65ac34513b3c9dc9
daniel love <danlove99@hotmail.co.uk>
2020-03-05 12:52:59 +0000

    Added JS Alogrithms

---
+++ jsbubblesort.py
@@ -0,0 +1,21 @@
+import js2py
+
+js = """
+const bubbleSort = arr => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+"""
+
+result = js2py.eval_js(arr) 
