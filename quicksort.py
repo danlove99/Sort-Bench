f7028fb7a98683fcce9814e03dc7ed4740638cd6
Daniel Love <danlove99@hotmail.co.uk>
2020-03-26 19:40:38 +0000

    Update quicksort.py
    Shortened the amount of lines by using one line if statements

--- quicksort.py
+++ quicksort.py
@@ -1,15 +1,11 @@
 def quicksort(arr):
     length = len(arr)
-    if length <= 1:
-        return arr
-    else:
-        pivot = arr.pop()
+    if length <= 1: return arr
+    else: pivot = arr.pop()
     greater = []
     lesser = []
     for i in arr:
-        if i > pivot:
-            greater.append(i)
-        else:
-            lesser.append(i)
+        if i > pivot: greater.append(i)
+        else: lesser.append(i)
     return quicksort(lesser) + [pivot] + quicksort(greater)
     
